1833515cc029404000879befd09ceb91d8c0f6ca
Carl Woodward <cjwoodward@gmail.com>
2010-01-30 00:44:33 +1100

    Added players

--- gamejam.rb
+++ gamejam.rb
@@ -3,8 +3,16 @@ require 'chingu'
 require 'angle_velocity'
 
 class Gamejam < Chingu::Window
+  class << self
+    def width; 960; end
+    def height; 640; end
+    def center
+      [width / 2, height / 2]
+    end
+  end
+  
   def initialize
-    super
+    super(Gamejam.width, Gamejam.height)
     @world = World.create
     @player = Player.create
     @world.input = {:holding_right => :start_rotation, :released_right => :finish_rotation}
@@ -16,8 +24,8 @@ class World < Chingu::GameObject
   has_traits :angle_velocity, :timer
   
   def initialize(options={})
-    super options.merge(:image => Gosu::Image['assets/world.png'])
-    self.x, self.y = 400, 300
+    super options.merge(:image => Gosu::Image['assets/blue.png'])
+    self.x, self.y = Gamejam.center
   end
   
   def start_rotation
@@ -32,9 +40,16 @@ end
 
 class Player < Chingu::GameObject
   def initialize(options={})
-    super options.merge(:image => Gosu::Image['assets/player.png'])
-    self.x, self.y = 400, 300
+    super options.merge(:image => Gosu::Image['assets/king.png'])
+    self.x, self.y = Gamejam.center
   end
 end
+
+# class Enemy < Chingu::GameObject
+#   def initialize(options={})
+#     super options.merge(:image => Gosu::Image['assets/player.png'])
+#     self.x, self.y = 400, 300
+#   end
+# end
 
 Gamejam.new.show
