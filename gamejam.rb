93088260a0466fb5b614596f28b38edc0db4dd93
Chris Lloyd <christopher.lloyd@gmail.com>
2010-01-30 01:46:54 +1100

    King drawing (without health).

--- gamejam.rb
+++ gamejam.rb
@@ -10,11 +10,12 @@ class Gamejam < Chingu::Window
       [width / 2, height / 2]
     end
   end
-  
+
   def initialize
-    super(Gamejam.width, Gamejam.height)
+    super(self.class.width, self.class.height)
     @world = World.create
-    @player = Player.create
+    @king = King.create
+    @crown = Crown.create
     @world.input = {:holding_right => :start_rotation, :released_right => :finish_rotation}
     self.input = {:escape => :close}
   end
@@ -22,34 +23,36 @@ end
 
 class World < Chingu::GameObject
   has_traits :angle_velocity, :timer
-  
+
   def initialize(options={})
     super options.merge(:image => Gosu::Image['assets/blue.png'])
     self.x, self.y = Gamejam.center
   end
-  
+
   def start_rotation
     stop_timers
     self.acceleration_angle = 0.075
   end
-  
+
   def finish_rotation
     during(2000) { self.acceleration_angle *= 0.95; self.velocity_angle *= 0.95 }.then { self.acceleration_angle = 0; self.velocity_angle = 0 }
   end
 end
 
-class Player < Chingu::GameObject
+class King < Chingu::GameObject
+  has_trait :bounding_circle
   def initialize(options={})
     super options.merge(:image => Gosu::Image['assets/king.png'])
     self.x, self.y = Gamejam.center
+    self.factor = 30.0 / 2310
   end
 end
 
-# class Enemy < Chingu::GameObject
-#   def initialize(options={})
-#     super options.merge(:image => Gosu::Image['assets/player.png'])
-#     self.x, self.y = 400, 300
-#   end
-# end
+class Crown < Chingu::GameObject
+  def initialize(options={})
+    super options.merge(:image => Gosu::Image['assets/crown.png'])
+    self.x, self.y = Gamejam.center
+  end
+end
 
 Gamejam.new.show
